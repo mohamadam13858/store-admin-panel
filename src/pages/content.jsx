62c623b61ff09ff980dd5c557d67000e6b3d45ea
mohamad <mh711748@gmail.com>
2024-12-23 21:45:03 +0330

    React-Router

--- src/pages/content.jsx
+++ src/pages/content.jsx
@@ -3,14 +3,27 @@ import { AdminContext } from '../context/adminLayoutContext';
 import Category from './category/Category';
 import Dashboard from './dashboard/Dashboard';
 import Product from './product/Product';
+import { Route, Router, Routes } from 'react-router-dom';
 
 const Content = () => {
     const {showSidebar} = useContext(AdminContext)
     return (
         <section id="content_section" 
         className={`bg-light py-2 px-3 ${showSidebar ? "with_sidebar" : null}`}>
+          <Routes>
+            <Route path='/' element={<Dashboard/>}/>
+            <Route path='Category' element={<Category/>}/>
+            <Route path='Product' element={<Product/>}/>
+
+
+
+
+
+
+            <Route path='*' element={<Dashboard/>}/>
+          </Routes>
           {/* <Dashboard/> */}
-          <Category/>
+          {/* <Category/> */}
           {/* <Product/> */}
         </section>
     );
