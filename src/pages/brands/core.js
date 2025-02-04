1b7f1635ab6aba4c73fe67c198bfaf3804798387
mohamad <mh711748@gmail.com>
2025-02-04 14:05:18 +0330

    addBrands

--- src/pages/brands/core.js
+++ src/pages/brands/core.js
@@ -8,16 +8,19 @@ export const initialValues = {
     original_name: "",
     persian_name: "",
     descriptions: "",
-    logo: null,
+    logo: "",
 };
 
 
 
-export const onSubmit = async ({values , actions , setData}) => {
+export const onSubmit = async (values , actions , setData) => {
     const res = await addNewBrandService(values);
+    console.log(res);
+    
     if (res.status === 201) {
         Alert("انجام شد" , res.data.message , "success")
         setData(lastData => [...lastData , res.data.data])
+        actions.resetForm()
     }
 }
 
@@ -25,14 +28,16 @@ export const onSubmit = async ({values , actions , setData}) => {
 
 export const validationSchema = Yup.object({
     original_name: Yup.string().required("لطفا این قسمت را پر کنید")
-        .matches(/^[\u0600-\u06FF\sA-Za-z0-9@!%$?&]+$/, "لطفا از حروف و اعداد استفاده کنید"),
+    .matches(/^[\u0600-\u06FF\sA-Za-z0-9@!%$?&]+$/, "لطفا از حروف و اعداد استفاده کنید"),
     persian_name: Yup.string()
-        .matches(/^[\u0600-\u06FF\sA-Za-z0-9@!%$?&]+$/, "لطفا از حروف و اعداد استفاده کنید"),
+    .matches(/^[\u0600-\u06FF\sA-Za-z0-9@!%$?&]+$/, "لطفا از حروف و اعداد استفاده کنید"),
     descriptions: Yup.string()
-        .matches(/^[\u0600-\u06FF\sA-Za-z0-9@!%$?&]+$/, "لطفا از حروف و اعداد استفاده کنید"),
+    .matches(/^[\u0600-\u06FF\sA-Za-z0-9@!%$?&]+$/, "لطفا از حروف و اعداد استفاده کنید"),
     logo: Yup.mixed()
-        .test("filesize", "حجم فایل نمیتواند بیشتر 500 کیلوبایت باشد", (value) =>
-            !value ? true : value.size <= 500 * 1024)
-        .test("format", "فرمت فایل باید jpg باشد", (value) =>
-            !value ? true : value.type === "image/jpeg" || value.type === "image/png")
+    .test("filesize", "حجم فایل نمیتواند بیشتر 500 کیلوبایت باشد", (value) =>
+        !value ? true : value.size <= 500 * 1024)
+    .test("format", "فرمت فایل باید jpg باشد", (value) =>
+        !value ? true : value.type === "image/jpeg" || value.type === "image/png")
 })
+
+    
